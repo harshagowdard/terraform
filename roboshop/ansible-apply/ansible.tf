a42ba8d2266caceff21204d7740dcfbb3328d6b1
Harshavardhana Rukminiyamma Devegowda <harshavardhana.rukminiyamma.devegowda@sap.com>
2021-06-20 22:00:21 +0530

    Adding terraform repo files

--- roboshop/ansible-apply/ansible.tf
+++ roboshop/ansible-apply/ansible.tf
@@ -6,20 +6,20 @@ resource "null_resource" "ansible-apply" {
   count = length(var.COMPONENTS)
   provisioner "remote-exec" {
 
-    connection {
-      host = "${element(var.COMPONENTS, count.index )}.roboshop.internal"
-      user = "centos"
-      password = "DevOps321"
-    }
+      connection {
+        host = "${element(var.COMPONENTS, count.index )}.roboshop.internal"
+        user = "centos"
+        password = "DevOps321"
+      }
 
-    inline = [
-      "sudo yum instance ansible -y",
-      "sudo yum remove ansible -y",
-      "sudo rm -rf /usr/lib/python2.7/site-packages/ansible*",
-      "sudo pip install ansible",
-      "ansible-pull -i localhost, -U https://github.com/harshagowdard/ansible.git roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
-    ]
-  }
+      inline = [
+        "sudo yum instance ansible -y",
+        "sudo yum remove ansible -y",
+        "sudo rm -rf /usr/lib/python2.7/site-packages/ansible*",
+        "sudo pip install ansible",
+        "ansible-pull -i localhost, -U https://github.com/harshagowdard/ansible.git roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
+      ]
+    }
 
 }
 
